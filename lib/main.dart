ac816ffe636ef8155f66029e6c11a1f0377e20a6
Sharath Kumar <1999sharathkumar7@gmail.com>
2023-12-03 12:54:01 +0530

    deviceorientation is set to portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:the_meal_app/views/home_screen.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp(),),);
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
